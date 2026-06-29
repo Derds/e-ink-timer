0037fb3af5c8e2062329f419b6bbd6d9fca9682a
derds <dervlaob@gmail.com>
2026-06-29 20:32:55 +0100

    update with led blink

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 """Minimal entry point for the e-ink timer app."""
 import app
+import machine
+import time
+
+led = machine.Pin('LED', machine.Pin.OUT)
+
 
 # Set RUN_MODE to 'run' to start the full app,
 # or 'test' to show individual screens for troubleshooting.
 RUN_MODE = 'run'
 
 if __name__ == '__main__':
+  
+    # blink LED
+    led.toggle()
+    time.sleep(2)
+    led.toggle()
+   
     application = app.App()
     if RUN_MODE == 'test':
         application.test_display()
